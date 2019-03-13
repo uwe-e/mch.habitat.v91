6c78efe980802f9fb0a3619395f1acbb6e12134d
uwe-e <git.bsetunes@outlook.com>
2019-03-13 12:57:52 +0100

    Forms MCH Content DynamicDataSource Field changed

---
+++ src/Feature/Forms/code/Models/Fields/MCHContentViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.ExperienceForms.Mvc.Models;
+
+namespace MCH.Feature.Forms.Models.Fields
+{
+    public class MCHContentViewModel : Sitecore.ExperienceForms.Mvc.Models.Fields.DropDownListViewModel
+    {
+        protected override void InitItemProperties(Item item)
+        {
+            base.InitItemProperties(item);
+            DataSource = item.Fields["Datasource"]?.Value;
+            IsDynamic = MainUtil.GetBool(item.Fields["Is Dynamic"]?.Value, defaultValue: false);
+            ShowEmptyItem = MainUtil.GetBool(item.Fields[Templates.MCHCountries.Fields.ShowEmptyItem]?.Value, defaultValue: false);
+        }
+        protected override void UpdateItemFields(Item item)
+        {
+            Assert.ArgumentNotNull(item, "item");
+            base.UpdateItemFields(item);
+            item.Fields[Templates.MCHCountries.Fields.ShowEmptyItem]?.SetValue(ShowEmptyItem ? "1" : string.Empty, force: true);
+        }
+
+        protected override void InitializeDataSourceSettings(Item item)
+        {
+            Assert.ArgumentNotNull(item, "item");
+            ListFieldItemCollection settings = DataSourceSettingsManager.GetSettings(item);
+            if (settings != null)
+            {
+                var fields = settings.OrderBy(c => c.Text);
+                Items.Clear();
+                Items.AddRange(fields);
+            }
+        }
+    }
+}
---
+++ src/Feature/Forms/code/Templates.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MCH.Feature.Forms
+{
+    public struct Templates
+    {
+        public struct MCHCountries
+        {
+            public static readonly ID ID = new ID("{488E6CF0-51A6-485F-B41B-24C785EF6EB0}");
+            public struct Fields
+            {
+                public static readonly ID ShowEmptyItem = new ID("{8DC6E4B4-3A7D-4C8E-8DE0-B04246AB57B7}");
+            }
+        }
+    }
+}
